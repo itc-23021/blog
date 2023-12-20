5bdb2a5de620c1b038e79dbe9dc38e73d7e9d3cf
itc-23021 <s23021@std.it-college.ac.jp>
2023-12-20 04:28:29 +0000

    p117

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,10 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
-export default function About () {
-  return <Hero title='Blog' subtitle='Recent Posts' />
+export default function Blog () {
+  return (
+    <Container>
+      <Hero title='Blog' subtitle='Recent Posts' />
+    </Container>
+  )
 }
