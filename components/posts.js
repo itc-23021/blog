6253d42733b028d387b29b7ff8ddad5056bd06f9
itc-23021 <s23021@std.it-college.ac.jp>
2024-01-30 06:29:04 +0000

    p273

--- components/posts.js
+++ components/posts.js
@@ -1,13 +1,25 @@
 import styles from 'styles/posts.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Posts ({ posts }) {
   return (
     <div className={styles.gridContainer}>
-      {posts.map(({ title, slug }) => (
+      {posts.map(({ title, slug, eyecatch }) => (
         <article className={styles.post} key={slug}>
           <Link href={`/blog/${slug}`}>
             <div>
+              <figure>
+                <Image
+                  src={eyecatch.url}
+                  alt=''
+                  layout='responsive'
+                  width={eyecatch.width}
+                  height={eyecatch.height}
+                  placeholder='blur'
+                  blurDataURL={eyecatch.blurDataURL}
+                />
+              </figure>
               <h2>{title}</h2>
             </div>
           </Link>
